5bfbedbb9f6b96bbf1342908f30b26b80927f180
Luan Lordello <lordellu@hotmail.com>
2024-08-15 21:37:13 -0300

    configurado conexao com rabbitMQ no projeto de pagamentos e enviando mensagem

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -1,19 +1,31 @@
 package br.com.alurafood.pagamentos.controller;
 
-import br.com.alurafood.pagamentos.dto.PagamentoDto;
-import br.com.alurafood.pagamentos.service.PagamentoService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import java.net.URI;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.net.URI;
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -21,6 +33,9 @@ public class PagamentoController {
 
     @Autowired
     private PagamentoService service;
+    
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @GetMapping
     public Page<PagamentoDto> listar(@PageableDefault(size = 10) Pageable paginacao) {
@@ -40,6 +55,10 @@ public class PagamentoController {
         PagamentoDto pagamento = service.criarPagamento(dto);
         URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
 
+        // envia mensagem pra o rabbitMQ
+        Message message = new Message(("Criei um pagamento com o id " + pagamento.getId()).getBytes());
+        rabbitTemplate.send("pagamento.concluido", message);
+        
         return ResponseEntity.created(endereco).body(pagamento);
     }
 
